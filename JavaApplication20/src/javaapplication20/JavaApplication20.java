971a1e3bc6a40979197ad3457b9387857986ae45
Marbe <marbeitzel@gmail.com>
2024-02-21 09:43:23 -0600

    Se realizo la rama develop

---
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -0,0 +1,12 @@
+package javaapplication20;
+
+public class JavaApplication20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
